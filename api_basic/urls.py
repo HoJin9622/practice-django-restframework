76b796b6380ff4bd0fa59c7d3a23e01c3c650f8e
HoJin9622 <kiss0104040@gmail.com>
2020-09-30 16:26:42 +0900

    4 - REST Framework Function Based API Views

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
-from .views import article_list
+from .views import article_detail, article_list
 
-urlpatterns = [path("article/", article_list)]
+urlpatterns = [path("article/", article_list), path("detail/<int:pk>/", article_detail)]
